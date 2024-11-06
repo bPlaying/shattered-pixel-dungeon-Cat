71b57cefd069f09ea9633e377e379e961690170c
bPlaying <webmaster@bplaying.com>
2024-11-06 12:23:24 +0100

    Add files via upload

---
+++ android/build/generated/source/buildConfig/androidTest/debug/com/shatteredpixel/shatteredpixeldungeon/android/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.shatteredpixel.shatteredpixeldungeon.android.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.bPlaying.shatteredpixeldungeon.indev.test";
+  public static final String BUILD_TYPE = "debug";
+}
---
+++ android/build/generated/source/buildConfig/debug/com/shatteredpixel/shatteredpixeldungeon/android/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.shatteredpixel.shatteredpixeldungeon.android;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.bPlaying.shatteredpixeldungeon.indev";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 628;
+  public static final String VERSION_NAME = "1.2.3-INDEV";
+}
